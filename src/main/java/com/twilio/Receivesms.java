6ae57c1ee31f7abf9b04823dbf1dfec8efa6c84d
Lubaba <lubaba070@gmail.com>
2017-06-30 14:45:49 -0400

    twilio

---
+++ src/main/java/com/twilio/Receivesms.java
@@ -0,0 +1,32 @@
+package com.twilio;
+
+import static spark.Spark.post;
+
+import static spark.Spark.*;
+import com.twilio.twiml.Body;
+import com.twilio.twiml.Message;
+import com.twilio.twiml.MessagingResponse;
+
+public class Receivesms {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//https://29ccde89.ngork.io/receive-sms
+		get("/", (req, res) -> "Hello, World");
+		post("/receive-sms", (req,res) -> {
+			Message sms = new Message.Builder()
+					.body(new Body("Is there any way you can come here?"))
+					.build();
+			MessagingResponse twiml = new MessagingResponse.Builder()
+					.message(sms)
+					.build();
+			
+			return twiml.toXml();
+			
+			
+		});
+
+	}
+
+}
