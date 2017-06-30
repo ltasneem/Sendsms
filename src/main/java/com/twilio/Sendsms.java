f080bd88765fadb44ef2d3a566a153896d40ae60
Lubaba <lubaba070@gmail.com>
2017-06-30 16:06:17 -0400

    removed info

--- src/main/java/com/twilio/Sendsms.java
+++ src/main/java/com/twilio/Sendsms.java
@@ -9,14 +9,15 @@ import com.twilio.type.PhoneNumber;
 //http://www.journaldev.com/8118/gradle-eclipse-plugin-tutorial
 
 
+
 public class Sendsms {
-	public static final String Account_sid = System.getenv("AC82813d309984d60c6e299f503627443b");
-	public static final String Auth_token = System.getenv("b66ed61f6eca8e5131392ded9f6bc9b4");
+	public static final String Account_sid = System.getenv("A");
+	public static final String Auth_token = System.getenv("b");
 	
 	public static void main(String [] args)
 	{
 		Twilio.init(Account_sid, Auth_token);
-		//Twilio.init("AC82813d309984d60c6e299f503627443b", "b66ed61f6eca8e5131392ded9f6bc9b4");
+		//
 		Message message = Message.creator( 
 						new PhoneNumber("+15712143827"),new PhoneNumber("+12408984566"),
 						"This is where I am").setMediaUrl("https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg")
