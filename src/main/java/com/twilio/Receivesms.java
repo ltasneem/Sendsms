0586d05304bf72e3771f6251f748472fd5ac9f0d
Lubaba <lubaba070@gmail.com>
2017-07-03 15:45:08 -0400

    changes done

--- src/main/java/com/twilio/Receivesms.java
+++ src/main/java/com/twilio/Receivesms.java
@@ -16,16 +16,32 @@ public class Receivesms {
 		//https://29ccde89.ngork.io/receive-sms
 		get("/", (req, res) -> "Hello, World");
 		post("/receive-sms", (req,res) -> {
-			Message sms = new Message.Builder()
+			/*Message sms = new Message.Builder()
 					.body(new Body("Is there any way you can come here?"))
+					.build();*/
+			String s= req.body();
+			String[] splitArray = s.split("&");
+			int i=0;
+			for(;i<splitArray.length;i++)
+			{
+				if(splitArray[i].contains("Body="))
+					break;
+			}
+			String final_s= splitArray[i].replace("Body="," ");
+			final_s = final_s.replace("+"," ");
+			Message sms = new Message.Builder()
+					.body(new Body(final_s))
 					.build();
 			MessagingResponse twiml = new MessagingResponse.Builder()
 					.message(sms)
 					.build();
+
+			System.out.println(s);
+
 			
 			return twiml.toXml();
-			
-			
+
+
 		});
 
 	}
